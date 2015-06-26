704b82e98680f6b86401b82da5549218b4b267d5
Chris Edwards <cedwards@H6497.ad.hugeinc.com>
2015-06-26 13:34:57 -0400

    setting up a test suite

---
+++ tests/server_spec.js
@@ -0,0 +1,37 @@
+var should = require('should');
+var request = require('supertest');
+var sinon = require('sinon');
+var server = require('../server');
+
+describe('Web Server', function () {
+
+    it('Handles 404s', function (done) {
+        request(server)
+            .get('/bork')
+            .expect(404)
+            .end(function (err, res) {
+                should.not.exist(err);
+                res.text.should.be.a.String;
+                return done();
+            });
+    });
+
+    it('Displays an error page for 500s', function (done) {
+
+        sinon.stub(server, 'get', function () {
+            new Error('break damnit!');
+        });
+
+        request(server)
+            .get('/')
+            .expect(500)
+            .end(function (err, res) {
+                should.not.exist(err);
+                res.text.should.be.a.String;
+                server.get.restore();
+                return done();
+            });
+
+    });
+
+});
