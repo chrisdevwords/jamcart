4d0a6276bce93c354c904bc0f2133cdfa1da4e9f
chrisdevwords <chrisdevwords@yahoo.com>
2015-06-27 12:39:08 -0400

    spotify track model now parses json through constructor as well as async api calls

--- app/lib/spotify-track.js
+++ app/lib/spotify-track.js
@@ -3,21 +3,43 @@ var _ = require('underscore');
 var request = require('request');
 var path = require('path');
 
-function SpotifyTrack (uri, requestedBy) {
+function SpotifyTrack (data, requestedBy) {
 
-    var _id = this.parseId(uri);
+    var _uri = this.parseUri(data);
+    var _id = this.parseId(_uri);
+    var _this = this;
 
     return _.extend(this, {
         get requestedBy () {
             return requestedBy;
         },
         get uri () {
-            return uri;
+            return _uri;
         },
         get id () {
             return _id;
+        },
+        get albumName () {
+            return _this.getAlbumName();
+        },
+        get artistName () {
+            return _this.getArtistName();
         }
     });
+};
+
+SpotifyTrack.prototype.getAlbumName = function() {
+    if (this.album) {
+        return this.album.name;
+    }
+    return undefined;
+};
+
+SpotifyTrack.prototype.getArtistName = function() {
+    if (_.isArray(this.artists)) {
+        return _.pluck(this.artists, 'name').join(', ');
+    }
+    return undefined;
 };
 
 SpotifyTrack.prototype.getInfo = function () {
@@ -32,16 +54,43 @@ SpotifyTrack.prototype.getInfo = function () {
                 if (err) {
                     reject(err);
                 } else {
-                    data = JSON.parse(body);
+                    data = _this.parseInfo(body);
                     if (data.error) {
                         reject(new Error(data.error.message));
                     } else {
-                        _this.data = data;
-                        resolve(_this.data);
+                        _this.info = data;
+                        resolve(_this.info);
                     }
                 }
             });
     });
+};
+
+SpotifyTrack.prototype.parseUri = function (data) {
+
+    var obj;
+
+    if (typeof data === 'string') {
+        obj = this.parseInfo(data);
+        if (obj.error) {
+            return data;
+        }
+    } else {
+        obj = data;
+    }
+
+    return obj.uri;
+};
+
+SpotifyTrack.prototype.parseInfo = function (data) {
+    var info;
+    try {
+        info = JSON.parse(data);
+        _.extend(this, info);
+    } catch (err) {
+        return {error: {message: err.message}};
+    }
+    return info;
 };
 
 SpotifyTrack.prototype.parseId = function (uri) {
